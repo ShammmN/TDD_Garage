7fe346fbbf027946da9b90ed55503f140a5b4ec4
ShammmN <shamnmretc@gmail.com>
2026-04-23 08:16:53 -0400

    added garage full in enetr_garagfe

--- garage.py
+++ garage.py
@@ -1,7 +1,8 @@
 def enter_garage(garage, car_id, entry_hour):
     if car_id in garage["cars"]:
         raise ValueError("Car is already in garage!")
-
+    if len(garage["cars"]) >= garage["capacity"]:
+        raise ValueError("Garage is full")
 def exit_garage(garage, car_id):
     pass
 
